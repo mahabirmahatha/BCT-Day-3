5cd3c1de700be85b2fe7b1d8a28c322f185fec0d
Mahabir Mahatha <164929944+mahabirmahatha@users.noreply.github.com>
2025-02-26 22:56:45 +0530

    Add files via upload

---
+++ lambda_greater.py
@@ -0,0 +1,3 @@
+print("enter two number: ")
+r=(lambda a,b:a if a>b else b)(int(input()),int(input()))
+print("the greater number is: ",r)
---
+++ sum_lambda.py
@@ -0,0 +1,5 @@
+s=lambda a,b:a+b
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+r=s(a,b)
+print(r)
